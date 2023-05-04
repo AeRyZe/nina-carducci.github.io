8eb4033528a50430b8292d9393d4d9266cc13812
AeRyZe <laurent.charleval@free.fr>
2023-05-04 05:28:17 +0200

    fix(index&js): added aria-labels & roles

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -218,10 +218,10 @@ $(document).ready(function () {
       },
       showItemTags(gallery, position, tags) {
         var tagItems =
-          '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
+          '<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all" aria-label="filter">Tous</span></li>';
         $.each(tags, function (index, value) {
           tagItems += `<li class="nav-item active">
-                  <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+                  <span class="nav-link" data-images-toggle="${value}" aria-label="filter">${value}</span></li>`;
         });
         var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
 
